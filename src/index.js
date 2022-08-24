dc7141093dc04814684f81ac28770d4b901d36a1
harshalpatil655 <harshalpatil655@gmail.com>
2022-08-24 19:13:53 +0530

    added

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./Redux/AppReducer/store";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   </BrowserRouter>
 );
