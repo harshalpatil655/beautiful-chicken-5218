ebc9537c5a183dc88dad0a68ec110f1e79ac058b
anushaAnu333 <97461362+anushaAnu333@users.noreply.github.com>
2022-08-25 21:21:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
-import {Navbar} from "./Components/Navbar";
+import { Navbar } from "./Components/Navbar";
 import MainRoute from "./Pages/MainRoute";
-import Shopping from "./Pages/Shopping";
-import TopOffers from "./Pages/TopOffers";
-
 
 function App() {
   return (
     <div className="App">
-       <Navbar />
+      <Navbar />
       <MainRoute />
- 
     </div>
   );
 }
